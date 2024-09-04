3a2c81211fe94718bf0abb02deb09ee833e436b2
llh4github <lilinhong_coding@foxmail.com>
2024-09-04 17:54:47 +0800

    fix: bean注册错误

--- lotus-web-security/src/main/kotlin/BeanRegister.kt
+++ lotus-web-security/src/main/kotlin/BeanRegister.kt
@@ -1,8 +1,11 @@
 package io.github.llh4github.lotus.websecurity
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.llh4github.lotus.commons.property.SecurityProperty
 import io.github.llh4github.lotus.commons.util.IdGenerator
+import io.github.llh4github.lotus.websecurity.security.JwtFilter
 import io.github.llh4github.lotus.websecurity.security.SpringSecurityConfig
+import io.github.llh4github.lotus.websecurity.service.JwtService
 import io.github.llh4github.lotus.websecurity.service.impl.JwtServiceImpl
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -19,7 +22,19 @@ class BeanRegister {
     }
 
     @Bean
-    fun springSecurityConfig(property: SecurityProperty) = SpringSecurityConfig(property)
+    @ConditionalOnMissingBean(name = ["springSecurityConfig"])
+    fun springSecurityConfig(
+        property: SecurityProperty,
+        objectMapper: ObjectMapper,
+        jwtFilter: JwtFilter,
+    ) = SpringSecurityConfig(property, objectMapper, jwtFilter)
+
+    @Bean
+    @ConditionalOnMissingBean(name = ["jwtFilter"])
+    fun jwtFilter(
+        jwtService: JwtService,
+        property: SecurityProperty,
+    ) = JwtFilter(jwtService, property)
 
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
